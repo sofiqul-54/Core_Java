efcc6a2036ce22071ec4387c5a8e0e7a7ed32cf9
sofiqul-54 <sofiqul54@gmail.com>
2018-09-07 19:00:27 +0600

    check_point chapter 01

---
+++ Resource/Solution of Java Book Practice/Chapter_01/Exercise_02/Exercise_01_02.java
@@ -0,0 +1,19 @@
+/**
+ * Exercise 1.2 from the book Introduction to Java Programming (10th).
+ * <p>
+ * Exercise instructions:<br>
+ * Write a program that displays "Welcome to Java" five times.
+ *
+ * @author Henrik Samuelsson, henrik.samuelsson(at)gmail.com
+ */
+public class Exercise_01_02 {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Java");
+		System.out.println("Welcome to Java");
+		System.out.println("Welcome to Java");
+		System.out.println("Welcome to Java");
+		System.out.println("Welcome to Java");
+	}
+
+}

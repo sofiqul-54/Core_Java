3e43dd007e5ea10afaef64a519c59ae5e93bec01
sofiqul-54 <sofiqul54@gmail.com>
2018-09-13 11:22:51 +0600

    j2ee

---
+++ Java_Work/chapter_6/src/Method/MethodEx1.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Method;
+
+/**
+ *
+ * @author User
+ */
+public class MethodEx1 {
+
+    public static void main(String[] args) {
+
+    }
+
+    public void display1() {
+        System.out.println("");
+    }
+
+    public int display2() {
+        int x = 10;
+        return x;
+    }
+    public String display3(){
+    return "Hello World";
+    }
+    
+
+    
+    
+}

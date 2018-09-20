cc1d1c9102df7281969ab09425983cd212025b25
sofiqul-54 <sofiqul54@gmail.com>
2018-09-20 13:12:03 +0600

    j2ee_20_9_2018_1

---
+++ Java_Work/chapter_10/src/isAhasA/Engine.java
@@ -0,0 +1,11 @@
+
+package isAhasA;
+public class Engine {
+    public void start(){
+        System.out.println("Started");
+    }
+    
+    public void stop(){
+        System.out.println("Stopped");
+    }
+}
---
+++ Java_Work/chapter_10/src/isAhasA/Test.java
@@ -0,0 +1,13 @@
+package isAhasA;
+
+public class Test {
+
+    public static void main(String[] args) {
+        Pulsar myPulsar = new Pulsar();
+        myPulsar.setColor("Black");
+        myPulsar.setMaxSpeed(136);
+        myPulsar.bikeInfo();
+        myPulsar.pulsarStartDemo();
+    }
+
+}

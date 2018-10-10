7d542786d79072af8b8edc8aa6a8efc68f729744
sofiqul-54 <sofiqul54@gmail.com>
2018-10-10 13:00:30 +0600

    j2eee

---
+++ Java_Work/chapter_24/src/com/sofiqul54/ImplementingList_Stacks_Queues_PriorityQueus/MyAbstractList.java
@@ -0,0 +1,42 @@
+package com.sofiqul54.ImplementingList_Stacks_Queues_PriorityQueus;
+
+public abstract class MyAbstractList<E> implements MyList<E> {
+protected int size = 0;
+    protected MyAbstractList() {
+
+    }
+
+    protected MyAbstractList(E[] objects) {
+        for (int i = 0; i < objects.length; i++) {
+            add(objects[i]);
+        }
+    }
+    
+    @Override
+    public void add(E e){
+    add(size, e);
+    }
+
+    @Override
+    public boolean isEmpty() {
+     return size ==0;
+    }
+
+   
+    @Override
+    public int size() {
+        return size;
+    }
+    
+     @Override
+    public boolean remove(E e) {
+       if(indexOf(e)>=0){
+           remove(indexOf(e));
+           return true;
+       }else
+           return false;
+    }
+    
+    
+    
+}
